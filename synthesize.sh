9eeebd7de7e1510131c4cf95cb3da8913ba91344
Patrick Edson <pedson@users.noreply.github.com>
2017-10-09 14:58:04 -0400

    Dev env typo.

--- synthesize.sh
+++ synthesize.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm --network devndb_back_tier -e NODE_ENV=production mouselightdatabrowser/data /apps/synthesize.sh $1 $2 $3
+docker run --rm --network ndb_back_tier -e NODE_ENV=production mouselightdatabrowser/data /apps/synthesize.sh $1 $2 $3
